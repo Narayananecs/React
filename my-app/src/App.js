35b89cc6bc4779041ed833b991c441b09a711876
Narayananecs <narayanan679@gmail.com>
2022-09-03 13:34:03 +0530

    Passed dynamic data uing PROPS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,18 @@ import ExpenseItem from './components/ExpenseItem'
 import './App.css';
 
 function App() {
+
+  const expenses = [
+    {id:'1', title: 'Hotel', date: new Date(2022,9,15), amount: 780},
+    {id:'2', title: 'Petrol', date: new Date(2022,9,15), amount: 700},
+    {id:'3', title: 'Room', date: new Date(2022,9,15), amount: 780}
+  ]
   return (
     <div className="App">
       Intro to react
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} date={expenses[0].date} amount={expenses[0].amount}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} date={expenses[1].date} amount={expenses[1].amount}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} date={expenses[2].date} amount={expenses[2].amount}></ExpenseItem>
     </div>
   );
 }
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const date = new Date(2022,9,15);
-    const expenseItem = 'Hotel';
-    const amount = '299';
+function ExpenseItem(props) {
+    
     return (
         <div className='expense-item'>
-            <div>{date.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseItem}</h2>
-                <div className='expense-item__price'>{amount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>);
 }
