ab74f41848efd6a610de1274ed5feadade04da16
Narayananecs <narayanan679@gmail.com>
2022-09-04 19:41:58 +0530

    Added form

--- my-app/src/components/ExpenseForm.js
+++ my-app/src/components/ExpenseForm.js
@@ -1,22 +1,52 @@
+import { useState } from 'react'
 import './ExpenseForm.css'
 function ExpenseForm() {
+
+    const [enteredTitle, setTile] = useState('');
+    const [enteredAmount, setAmount] = useState('');
+    const [enteredDate, setDate] = useState('');
+
+    const titleHandler = (event) => {
+        setTile(event.target.value);
+    }
+
+    const amountHandler = (event) => {
+        setAmount(event.target.value);
+    }
+
+    const dateHandler = (event) => {
+        setDate(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const data  = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: enteredDate
+        }
+
+        console.log(data);
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleHandler}/>
                 </div>
                 <div className="new-expense__control">
                 <label>Amount</label>
-                    <input type="number" />
+                    <input type="number" onChange={amountHandler}/>
                 </div>
                 <div className="new-expense__control">
                 <label>Date</label>
-                    <input type="date" />
+                    <input type="date" onChange={dateHandler}/>
                 </div>
-                <button>Submit</button>
             </div>
+            <div className="new-expense__actions">
+                    <button type='submit'>Submit</button>
+                </div>
         </form>
     )
 }
