801b26695d675c4ac65c86baa7d9fa554296819d
Narayananecs <narayanan679@gmail.com>
2022-09-04 17:31:03 +0530

    Added event listener

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -2,12 +2,20 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 function ExpenseItem(props) {
+
+    let title  = props.title;
+
+    function change() {
+        title = "changee"
+        console.log(title);
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date = {props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
+                <button onClick={change}>Change</button>
             </div>
         </Card>);
 }
