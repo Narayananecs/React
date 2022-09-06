4877d47266b2bdb8ce080923941afb8319440f55
Narayananecs <narayanan679@gmail.com>
2022-09-06 09:55:34 +0530

    Add expense logic completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,29 @@ import './App.css';
 import './components/Expenses.css'
 import Card from './components/Card';
 import NewExpense from './components/NewExpense';
+import { useState } from 'react';
 
-
+let initialExpenses = [
+  {id:'1', title: 'Hotel', date: new Date(2022,9,15), amount: 780},
+  {id:'2', title: 'Petrol', date: new Date(2022,9,15), amount: 710},
+  {id:'3', title: 'Room', date: new Date(2022,9,15), amount: 730}
+]
 function App() {
 
-  const expenses = [
-    {id:'1', title: 'Hotel', date: new Date(2022,9,15), amount: 780},
-    {id:'2', title: 'Petrol', date: new Date(2022,9,15), amount: 700},
-    {id:'3', title: 'Room', date: new Date(2022,9,15), amount: 780}
-  ]
+  let [expense, setExpense] = useState(initialExpenses);
   const saveExpense = (data) =>{
     console.log(data);
+    data.id = Math.random().toString()
+    data.date = new Date(data.date)
+    setExpense(prevState => {
+      return [data, ...prevState];
+    });
   }
   return (
     <div>
     <NewExpense onSaveExpense={saveExpense}/>
     <Card className="expenses">
-      <Expenses data = {expenses}/>
+      <Expenses data = {expense}/>
     </Card>
     </div>
   );
--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,24 +1,14 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
-import { useState } from 'react';
 function ExpenseItem(props) {
-
-    let [title,changeValue]  = useState(props.title);
-
-    function change() {
-        changeValue("changee");
-        console.log(title);
-    }
-
     
     return (
         <Card className='expense-item'>
             <ExpenseDate date = {props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
-                <button onClick={change}>Change</button>
             </div>
         </Card>);
 }
